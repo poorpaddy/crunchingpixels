e92c373747bc39332a4e5d17793b73896a8ddad7
Suleman Khan <sulemank97@gmail.com>
2021-12-16 16:28:14 +0500

    Implement Container component responsivenes for all screen

--- frontend/src/components/layout/Container/index.tsx
+++ frontend/src/components/layout/Container/index.tsx
@@ -3,7 +3,16 @@ import cn from "classnames";
 import { Props } from "./types";
 
 const Container: React.FC<Props> = ({ children, className }) => {
-  return <div className={cn("px-4", className)}>{children}</div>;
+  return (
+    <div
+      className={cn(
+        "px-4 sm:px-8 md:px-12 lg:px-[120px] max-w-[1168px] mx-auto",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Container;
